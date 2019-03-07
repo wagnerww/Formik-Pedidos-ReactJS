8f094c8acf41c69ee80ce434448f87c5c09b4f4a
wagner <wagnerricardonet@gmail.com>
2019-03-07 17:54:34 -0300

    pedido funcional

--- src/pages/pedidosForm/index.js
+++ src/pages/pedidosForm/index.js
@@ -3,11 +3,26 @@ import { withFormik, FieldArray } from "formik";
 import * as Yup from "yup";
 
 import InputValidator from "../../commun/elements/inputValidator";
-import { push_uniq } from "terser";
 
 // import styles from './styles';
 
 class pedidosForm extends Component {
+  constructor() {
+    const produtos = {};
+  }
+
+  Subtotal = async (i, e) => {
+    const { handleChange } = this.props;
+    await handleChange(e);
+    const { setFieldValue, values } = this.props;
+    const { produtos } = values;
+    console.log("prod", produtos);
+    const qtd = produtos[i].qtd;
+    const total = qtd * 10;
+    await setFieldValue(`produtos[${i}].total`, total);
+    //onst produto = values.produtos[index];
+  };
+
   render() {
     const { handleSubmit, errors, handleChange, values, touched } = this.props;
 
@@ -49,69 +64,64 @@ class pedidosForm extends Component {
           name="produtos"
           render={({ push }) => (
             <Fragment>
-              <table>
-                <thead>
-                  <tr>
-                    <td>id</td>
-                    <td>descricao</td>
-                    <td>Quantidade</td>
-                    <td>Valor</td>
-                    <td>Total</td>
-                  </tr>
-                </thead>
-                <tbody>
-                  {values.produtos.map((prod, index) => (
-                    <tr key={index}>
-                      <td>
-                        <InputValidator
-                          type="number"
-                          placeholder="id"
-                          name={`produtos[${index}].id`}
-                          value={prod.id}
-                          handleChange={handleChange}
-                        />
-                      </td>
-                      <td>
-                        <InputValidator
-                          type="text"
-                          placeholder="descricao"
-                          name={`produtos[${index}].descricao`}
-                          value={prod.descricao}
-                          handleChange={handleChange}
-                        />
-                      </td>
-                      <td>
-                        <InputValidator
-                          type="text"
-                          placeholder="quantidade"
-                          name={`produtos[${index}].qtd`}
-                          value={prod.qtd}
-                          handleChange={handleChange}
-                        />
-                      </td>
-                      <td>
-                        <InputValidator
-                          type="number"
-                          placeholder="valor"
-                          name={`produtos[${index}].valor`}
-                          value={prod.valor}
-                          handleChange={handleChange}
-                        />
-                      </td>
-                      <td>
-                        <InputValidator
-                          type="text"
-                          placeholder="total"
-                          name={`produtos[${index}].total`}
-                          value={prod.total}
-                          handleChange={handleChange}
-                        />
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-              <button type="button" onClick={async () => await push({})}>
+              <div className="table tableCol5">
+                <div className="tableCell">id</div>
+                <div className="tableCell">descricao</div>
+                <div className="tableCell">Quantidade</div>
+                <div className="tableCell">Valor</div>
+                <div className="tableCell">Total</div>
+
+                {values.produtos.map((prod, index) => (
+                  <div key={index} className="table tableCol5">
+                    <div className="tableCell">
+                      <InputValidator
+                        type="number"
+                        placeholder="id"
+                        name={`produtos[${index}].id`}
+                        value={prod.id}
+                        handleChange={handleChange}
+                      />
+                    </div>
+                    <div className="tableCell">
+                      <InputValidator
+                        type="text"
+                        placeholder="descricao"
+                        name={`produtos[${index}].descricao`}
+                        value={prod.descricao}
+                        handleChange={handleChange}
+                      />
+                    </div>
+                    <div className="tableCell">
+                      <InputValidator
+                        type="text"
+                        placeholder="quantidade"
+                        name={`produtos[${index}].qtd`}
+                        value={prod.qtd}
+                        handleChange={e => this.Subtotal(index, e)}
+                      />
+                    </div>
+                    <div className="tableCell">
+                      <InputValidator
+                        type="number"
+                        placeholder="valor"
+                        name={`produtos[${index}].valor`}
+                        value={prod.valor}
+                        handleChange={handleChange}
+                      />
+                    </div>
+                    <div className="tableCell">
+                      <InputValidator
+                        type="text"
+                        placeholder="total"
+                        name={`produtos[${index}].total`}
+                        value={prod.total}
+                        handleChange={handleChange}
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <button type="button" onClick={async () => await push("")}>
                 Add
               </button>
             </Fragment>
