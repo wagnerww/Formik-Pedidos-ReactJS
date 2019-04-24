4744bff5344e13d45d4a87e662f1c01d4b8bfa66
Wagner Ricardo Wagner <wagnerricardonet@Gmail.com>
2019-04-24 16:53:08 -0300

    alterando o withformik

--- src/pages/Formik/CrudPedidosWithFormik/index.js
+++ src/pages/Formik/CrudPedidosWithFormik/index.js
@@ -8,79 +8,87 @@ import Input from "../../../Components/Input";
 
 import { Debug } from "../../../Components/services/Debug";
 
-class LoadForm extends Component {
-  state = {
-    id: 0,
-    cliente: "",
-    endereco: "",
-    produtos: [produtos],
-    total: 0
-  };
+const Campos = {
+  id: 0,
+  cliente: "wagner r wagner",
+  endereco: "",
+  produtos: [produtos],
+  total: 0
 }
 
-const pedidosForm = ({
-  errors,
-  handleChange,
-  values,
-  touched,
-  setFieldValue
-}) => (
-    <Form>
-      {!!errors.id && touched.id && <span>{errors.id}</span>}
-      <label htmlFor="">id</label>
-      <Input
-        type="text"
-        placeholder="id"
-        name="id"
-        onChange={handleChange}
-        value={values.id}
-        isValid={!!errors.id}
-      />
-      {!!errors.cliente && touched.cliente && <span>{errors.cliente}</span>}
-      <label htmlFor="">Cliente</label>
-      <Input
-        type="text"
-        placeholder="Cliente"
-        name="cliente"
-        onChange={handleChange}
-        value={values.cliente}
-        isValid={!!errors.cliente && touched.cliente}
-      />
-      {!!errors.endereco && touched.endereco && <span>{errors.endereco}</span>}
-      <label htmlFor="">Endereco</label>
-      <Input
-        type="text"
-        placeholder="Endereceço"
-        name="endereco"
-        onChange={handleChange}
-        value={values.endereco}
-        isValid={!!errors.endereco && touched.endereco}
-      />
-      <PedProdutos
-        values={values}
-        handleChange={handleChange}
-        produtos={produtos}
-        setFieldValue={setFieldValue}
-      />
+class LoadForm extends Component {
+  async componentDidMount() {
+    const { setValues } = this.props;
+    Campos.cliente = "wagner"
+    await setValues(
+      Campos
+    )
+  }
 
-      <Input
-        type="text"
-        placeholder="total"
-        name="total"
-        onChange={handleChange}
-        value={values.total}
-      />
+  render() {
+    const { errors,
+      handleChange,
+      values,
+      touched,
+      setFieldValue } = this.props;
+    return (
+      <Form>
+        {!!errors.id && touched.id && <span>{errors.id}</span>}
+        <label htmlFor="">id</label>
+        <Input
+          type="text"
+          placeholder="id"
+          name="id"
+          onChange={handleChange}
+          value={values.id}
+          isValid={!!errors.id}
+        />
+        {!!errors.cliente && touched.cliente && <span>{errors.cliente}</span>}
+        <label htmlFor="">Cliente</label>
+        <Input
+          type="text"
+          placeholder="Cliente"
+          name="cliente"
+          onChange={handleChange}
+          value={values.cliente}
+          isValid={!!errors.cliente && touched.cliente}
+        />
+        {!!errors.endereco && touched.endereco && <span>{errors.endereco}</span>}
+        <label htmlFor="">Endereco</label>
+        <Input
+          type="text"
+          placeholder="Endereceço"
+          name="endereco"
+          onChange={handleChange}
+          value={values.endereco}
+          isValid={!!errors.endereco && touched.endereco}
+        />
+        <PedProdutos
+          values={values}
+          handleChange={handleChange}
+          produtos={produtos}
+          setFieldValue={setFieldValue}
+        />
 
-      <button type="submit">Enviar</button>
-      <Debug />
-    </Form>
-  );
+        <Input
+          type="text"
+          placeholder="total"
+          name="total"
+          onChange={handleChange}
+          value={values.total}
+        />
+
+        <button type="submit">Enviar</button>
+        <Debug />
+      </Form>
+    )
+  }
+}
 
 export default withFormik({
-  mapPropsToValues: props => {
-    console.log("load", props);
-    return { LoadForm };
-  },
+  mapPropsToValues: props => (
+    Campos
+  ),
 
   validateOnBlur: false,
   validateOnChange: false,
@@ -95,4 +103,4 @@ export default withFormik({
     console.log("valor", values);
     //chamada a api
   }
-})(pedidosForm);
+})(LoadForm);
